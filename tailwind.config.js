bce2509b46256d465fe3a7475be4c69e3d42446a
Dhruv0369 <148555616+Dhruv0369@users.noreply.github.com>
2024-09-05 10:52:46 +0530

    Header Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // module.exports = {
 //   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
+//     "./src/**/*.{js,jsx,ts,tsx}",  // This should cover all JS, JSX, TS, and TSX files inside the src folder
+//     "./public/index.html",  // This includes the HTML file for the project
 //   ],
 //   theme: {
 //     extend: {},
