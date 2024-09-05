9626f73b06f30e53c697483cb27f9868158fdfb5
Dhruv0369 <148555616+Dhruv0369@users.noreply.github.com>
2024-09-05 14:22:47 +0530

    Responsive Header and Footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import logo from '../assets/logo.webp'
+import React from 'react';
+import logo from '../assets/logo.webp';
 
 const Navbar = () => {
     return (
         <div className='shadow-sm'>
-            <nav className="navbar navbar-expand-lg mx-auto" style={{ width: '63%', backgroundColor: 'white'}}>
+            <nav className="navbar navbar-expand-lg mx-auto" style={{ width: '63%', backgroundColor: 'white' }}>
 
                 <img src={logo} className='mr-3' alt="Get App Quick" style={{ height: '30px', width: '200px', fontWeight: 'bold' }} />
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,13 +19,13 @@ const Navbar = () => {
                             <a className="nav-link" href="#">Games</a>
                         </li>
                     </ul>
-                    <div className="ml-auto ">
-                        <div className="input-group border-0 " style={{ maxWidth: '24rem', width: '100%'}}>
-                            <span className="input-group-text bg-white inputsize" id="basic-addon1" style={{ borderRadius: '25px' ,border: '2px solid #252c4d'}}>
+                    <div className="ml-auto">
+                        <div className="input-group border-0" style={{ maxWidth: '24rem', width: '100%' }}>
+                            <span className="input-group-text bg-white inputsize" id="basic-addon1" style={{ borderRadius: '25px', border: '2px solid #252c4d' }}>
                                 <i className="fa-solid fa-magnifying-glass serach mr-2"></i>&nbsp;
                                 <input
                                     type="text"
-                                    className="border-0 p-1 inputcolor "
+                                    className="border-0 p-1 inputcolor"
                                     placeholder="Search for apps & games"
                                     aria-label="Search"
                                     aria-describedby="basic-addon1"
@@ -37,83 +37,7 @@ const Navbar = () => {
                 </div>
             </nav>
         </div>
-    )
+    );
 }
 
-export default Navbar
-
-
-// import React from 'react'
-// import logo from '../assets/logo.webp'
-
-// const Navbar = () => {
-//     return (
-//         <header className="header">
-//         <div className="header-all layout-minbox flex items-center">
-//           <a href="index.html" className="header-logobox">
-//             <img src={logo} alt="get-app-quick" className="header-logobox-logo" />
-//           </a>
-//           <a href="web/all-apps.html" className="header-app flex items-center">
-//             <div className="header-box flex items-center">
-              
-//               <div className="header-text">Apps</div>
-//             </div>
-//           </a>
-//           <a href="web/all-games.html" className="header-game flex items-center">
-//             <div className="header-box flex items-center">
-             
-//               <div className="header-text">Games</div>
-//             </div>
-//           </a>
-//           <a href="javascript:;" className="header-topics" style={{ display: 'none' }}>
-//             <div className="header-box flex items-center">
-             
-//               <div className="header-text">Topics</div>
-//             </div>
-//           </a>
-//           <div className="header-searchall flex items-center relative">
-//             <input type="checkbox" id="header-search" className="hidden" />
-//             <label htmlFor="header-search" className="header-searchicon">
-//             <i className="fa-solid fa-magnifying-glass serach mr-2"></i>
-//             </label>
-//             <div className="header-search absolute bg-white border border-gray-300 rounded-md p-2 hidden">
-//               <input
-//                 required
-//                 autoComplete="off"
-//                 id="searchInput"
-//                 type="text"
-//                 placeholder="Search for apps &amp; games"
-//                 name="q"
-//                 className="header-searchtext border-none p-2 rounded-md"
-//               />
-//               <div id="autocomplete-results" className="mt-2">
-//                 {/* Add a container for the suggestions below the input field */}
-//               </div>
-//             </div>
-//             <div className="header-search-serp">
-//               <form
-//                 action="https://getappsquick.us/search/"
-//                 className="header-search-serp-form flex items-center"
-//               >
-//                 <input
-//                   required
-//                   autoComplete="off"
-//                   type="text"
-//                   placeholder="Search for apps &amp; games"
-//                   name="q"
-//                   className="header-search-serp-input border-none p-2 rounded-md"
-//                 />
-//                 <button type="submit" className="header-search-serp-btn ml-2 p-2 bg-blue-500 text-white rounded-md">
-//                   <svg className="icon-search">
-//                     <use xlinkHref="#icon-go-search" />
-//                   </svg>
-//                 </button>
-//               </form>
-//             </div>
-//           </div>
-//         </div>
-//       </header>
-//     )
-// }
-
-// export default Navbar
+export default Navbar;
---
+++ src/components/Topgame.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo1 from '../assets/1@2x.jpg'
+const Topgame = () => {
+    return (
+        
+    )
+}
+
+export default Topgame
